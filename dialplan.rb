4cf55235f3f6dfb3847d4060e968b0df55f18108
Jay Phillips <jicksta+git@gmail.com>
2008-06-09 15:30:03 -0700

    Dialplan now implements the context names in the database

--- dialplan.rb
+++ dialplan.rb
@@ -3,3 +3,23 @@ adhearsion {
     instance_eval eval(context.name)
   end
 }
+
+greet {
+  play 'welcome'
+}
+
+please_wait {
+  play 'please-wait'
+}
+
+dial_jay {
+  dial 'SIP/some_trunk/14155244444'
+}
+
+jay_voicemail {
+  voicemail 1337
+}
+
+hangup {
+  hangup
+}
